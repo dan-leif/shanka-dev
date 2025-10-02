8bff2bb12cfbb67c8bb1c19f28354a2b2d462c9c
dan-leif <dan.joseph.moore@gmail.com>
2025-10-01 21:12:30 -0700

    Upload initial code. Skipping fonts.

---
+++ vocab-1.js
@@ -0,0 +1,306 @@
+/* vocab-1.js
+   Global vocab payload for 閃卡
+   Schema: window.VOCAB = { lessons: { "Lesson 1-1": [...], ... } }
+   Each item: { en, trad, simp, py }
+*/
+window.VOCAB = {
+  lessons: {
+    // ---- 1-1 ----
+    "Lesson 1-1": [
+      {en:"you", trad:"你", simp:"你", py:"nǐ"},
+      {en:"good; well", trad:"好", simp:"好", py:"hǎo"},
+      {en:"to be; yes (affirmative)", trad:"是", simp:"是", py:"shì"},
+      {en:"student", trad:"學生", simp:"学生", py:"xuéshēng"},
+      {en:"teacher", trad:"老師", simp:"老师", py:"lǎoshī"},
+      {en:"I; me", trad:"我", simp:"我", py:"wǒ"},
+      {en:"also; too", trad:"也", simp:"也", py:"yě"},
+      {en:"not; no", trad:"不", simp:"不", py:"bù"},
+      {en:"he; him", trad:"他", simp:"他", py:"tā"},
+      {en:"she; her", trad:"她", simp:"她", py:"tā"},
+      {en:"(yes/no question particle)", trad:"嗎", simp:"吗", py:"ma"},
+      {en:"(sentence particle: and you?)", trad:"呢", simp:"呢", py:"ne"}
+    ],
+
+    // ---- 1-2 (Names & basic classroom language) ----
+    "Lesson 1-2": [
+      {en:"(polite) you", trad:"您", simp:"您", py:"nín"},
+      {en:"honored; expensive", trad:"貴", simp:"贵", py:"guì"},
+      {en:"surname; be surnamed", trad:"姓", simp:"姓", py:"xìng"},
+      {en:"may I ask; (polite) please", trad:"請問", simp:"请问", py:"qǐngwèn"},
+      {en:"(structural particle)", trad:"的", simp:"的", py:"de"},
+      {en:"English (language)", trad:"英文", simp:"英文", py:"Yīngwén"},
+      {en:"name", trad:"名字", simp:"名字", py:"míngzi"},
+      {en:"Chinese (language)", trad:"中文", simp:"中文", py:"Zhōngwén"},
+      {en:"to be called; to call", trad:"叫", simp:"叫", py:"jiào"},
+      {en:"what", trad:"什麼", simp:"什么", py:"shénme"},
+      {en:"she; her", trad:"她", simp:"她", py:"tā"},
+      {en:"who; whom", trad:"誰", simp:"谁", py:"shéi / shuí"},
+      {en:"classmate", trad:"同學", simp:"同学", py:"tóngxué"}
+    ],
+
+    // ---- 1-3 (Nationalities & languages) ----
+    "Lesson 1-3": [
+      {en:"which", trad:"哪", simp:"哪", py:"nǎ / něi"},
+      {en:"country", trad:"國", simp:"国", py:"guó"},
+      {en:"person; people", trad:"人", simp:"人", py:"rén"},
+      {en:"very; quite", trad:"很", simp:"很", py:"hěn"},
+      {en:"by the way", trad:"對了", simp:"对了", py:"duìle"},
+      {en:"France", trad:"法國", simp:"法国", py:"Fǎguó"},
+      {en:"United States", trad:"美國", simp:"美国", py:"Měiguó"},
+      {en:"Britain", trad:"英國", simp:"英国", py:"Yīngguó"},
+      {en:"China", trad:"中國", simp:"中国", py:"Zhōngguó"},
+      {en:"to speak; say", trad:"說", simp:"说", py:"shuō"},
+      {en:"can; know how to", trad:"會", simp:"会", py:"huì"},
+      {en:"a little (retroflex)", trad:"一點兒", simp:"一点儿", py:"yìdiǎnr"},
+      {en:"French (language)", trad:"法文", simp:"法文", py:"Fǎwén"},
+      {en:"and", trad:"和", simp:"和", py:"hé"}
+    ],
+
+    // ---- 1-4 (Studies) ----
+    "Lesson 1-4": [
+      {en:"that", trad:"那", simp:"那", py:"nà / nèi"},
+      {en:"book", trad:"書", simp:"书", py:"shū"},
+      {en:"this", trad:"這", simp:"这", py:"zhè / zhèi"},
+      {en:"(M.W. for books) volume", trad:"本", simp:"本", py:"běn"},
+      {en:"literature", trad:"文學", simp:"文学", py:"wénxué"},
+      {en:"engineering", trad:"工程", simp:"工程", py:"gōngchéng"},
+      {en:"difficult", trad:"難", simp:"难", py:"nán"},
+      {en:"too; excessively", trad:"太", simp:"太", py:"tài"},
+      {en:"but; however", trad:"可是", simp:"可是", py:"kěshì"},
+      {en:"homework; assignment", trad:"功課", simp:"功课", py:"gōngkè"},
+      {en:"many; much", trad:"多", simp:"多", py:"duō"},
+      {en:"we; us", trad:"我們", simp:"我们", py:"wǒmen"},
+      {en:"few; little", trad:"少", simp:"少", py:"shǎo"}
+    ],
+
+    // ---- 1-5 (Introductions) ----
+    "Lesson 1-5": [
+      {en:"friend", trad:"朋友", simp:"朋友", py:"péngyou"},
+      {en:"to come", trad:"來", simp:"来", py:"lái"},
+      {en:"to introduce; introduction", trad:"介紹", simp:"介绍", py:"jièshào"},
+      {en:"(after verb) briefly", trad:"一下", simp:"一下", py:"yíxià"},
+      {en:"roommate", trad:"室友", simp:"室友", py:"shìyǒu"},
+      {en:"to have; there is/are", trad:"有", simp:"有", py:"yǒu"},
+      {en:"how many", trad:"幾", simp:"几", py:"jǐ"},
+      {en:"two (before M.W.)", trad:"兩", simp:"两", py:"liǎng"},
+      {en:"(general M.W.)", trad:"個", simp:"个", py:"gè"},
+      {en:"all; both", trad:"都", simp:"都", py:"dōu"},
+      {en:"often; frequently", trad:"常", simp:"常", py:"cháng"},
+      {en:"with", trad:"跟", simp:"跟", py:"gēn"}
+    ],
+
+    // ---- 1-6 (Family) ----
+    "Lesson 1-6": [
+      {en:"home; family", trad:"家", simp:"家", py:"jiā"},
+      {en:"everyone", trad:"大家", simp:"大家", py:"dàjiā"},
+      {en:"from", trad:"從", simp:"从", py:"cóng"},
+      {en:"to be at; to be in", trad:"在", simp:"在", py:"zài"},
+      {en:"one", trad:"一", simp:"一", py:"yī"},
+      {en:"four", trad:"四", simp:"四", py:"sì"},
+      {en:"father", trad:"爸爸", simp:"爸爸", py:"bàba"},
+      {en:"mother", trad:"媽媽", simp:"妈妈", py:"māma"},
+      {en:"elder sister", trad:"姊姊", simp:"姐姐", py:"jiějie"},
+      {en:"job; to work", trad:"工作", simp:"工作", py:"gōngzuò"},
+      {en:"engineer", trad:"工程師", simp:"工程师", py:"gōngchéngshī"},
+      {en:"boyfriend", trad:"男朋友", simp:"男朋友", py:"nánpéngyou"},
+      {en:"male", trad:"男", simp:"男", py:"nán"},
+      {en:"to not have; without", trad:"沒有", simp:"没有", py:"méiyǒu"},
+      {en:"(M.W. vehicles)", trad:"輛", simp:"辆", py:"liàng"},
+      {en:"car", trad:"車", simp:"车", py:"chē"},
+      {en:"(M.W. for some animals/one of a pair)", trad:"隻", simp:"只", py:"zhī"},
+      {en:"dog", trad:"狗", simp:"狗", py:"gǒu"},
+      {en:"to love", trad:"愛", simp:"爱", py:"ài"}
+    ],
+
+    // ---- 1-7 (Addresses & numbers) ----
+    "Lesson 1-7": [
+      {en:"to live", trad:"住", simp:"住", py:"zhù"},
+      {en:"where", trad:"哪兒", simp:"哪儿", py:"nǎr"},
+      {en:"dorm", trad:"宿舍", simp:"宿舍", py:"sùshè"},
+      {en:"how many; how much", trad:"多少", simp:"多少", py:"duōshǎo"},
+      {en:"number", trad:"號", simp:"号", py:"hào"},
+      {en:"room", trad:"房間", simp:"房间", py:"fángjiān"},
+      {en:"big", trad:"大", simp:"大", py:"dà"},
+      {en:"phone", trad:"電話", simp:"电话", py:"diànhuà"},
+      {en:"small", trad:"小", simp:"小", py:"xiǎo"},
+      {en:"number (serial) ", trad:"號碼", simp:"号码", py:"hàomǎ"},
+      {en:"two", trad:"二", simp:"二", py:"èr"},
+      {en:"three", trad:"三", simp:"三", py:"sān"},
+      {en:"five", trad:"五", simp:"五", py:"wǔ"},
+      {en:"six", trad:"六", simp:"六", py:"liù"},
+      {en:"seven", trad:"七", simp:"七", py:"qī"},
+      {en:"eight", trad:"八", simp:"八", py:"bā"},
+      {en:"nine", trad:"九", simp:"九", py:"jiǔ"},
+      {en:"cell phone", trad:"手機", simp:"手机", py:"shǒujī"},
+      {en:"off campus", trad:"校外", simp:"校外", py:"xiàowài"}
+    ],
+
+    // ---- 1-8 (Meeting & Making Plans) ----
+    "Lesson 1-8": [
+      {en:"to know; recognize; be acquainted/familiar with", trad:"認識", simp:"认识", py:"rènshi"},
+      {en:"to go", trad:"去", simp:"去", py:"qù"},
+      {en:"to attend class; class, lesson", trad:"上課", simp:"上课", py:"shàngkè"},
+      {en:"to end class; class dismissed", trad:"下課", simp:"下课", py:"xiàkè"},
+      {en:"after; afterwards; later", trad:"以後", simp:"以后", py:"yǐhòu"},
+      {en:"matter; thing; business", trad:"事", simp:"事", py:"shì"},
+      {en:"to want; to think; to miss", trad:"想", simp:"想", py:"xiǎng"},
+      {en:"to return", trad:"回", simp:"回", py:"huí"},
+      {en:"together", trad:"一起", simp:"一起", py:"yìqǐ"},
+      {en:"to eat; to have a meal", trad:"吃飯", simp:"吃饭", py:"chīfàn"},
+      {en:"to eat", trad:"吃", simp:"吃", py:"chī"},
+      {en:"cooked rice; meal", trad:"飯", simp:"饭", py:"fàn"},
+      {en:"dish; cuisine", trad:"菜", simp:"菜", py:"cài"},
+      {en:"today", trad:"今天", simp:"今天", py:"jīntiān"},
+      {en:"next time", trad:"下次", simp:"下次", py:"xiàcì"},
+      {en:"how is it; how about it", trad:"怎麼樣", simp:"怎么样", py:"zěnmeyàng"},
+      {en:"to be all right; be okay", trad:"行", simp:"行", py:"xíng"},
+      {en:"good-bye; see you again", trad:"再見", simp:"再见", py:"zàijiàn"},
+      {en:"to see", trad:"見", simp:"见", py:"jiàn"}
+    ],
+
+    // ---- 1-9 (Phone Calls) ----
+    "Lesson 1-9": [
+      {en:"(in the process of)", trad:"正在", simp:"正在", py:"zhèngzài"},
+      {en:"to make a phone call", trad:"打電話", simp:"打电话", py:"dǎ diànhuà"},
+      {en:"to hit; to call; to make", trad:"打", simp:"打", py:"dǎ"},
+      {en:"hello/hey (on the phone)", trad:"喂", simp:"喂", py:"wèi"},
+      {en:"to wait a moment; hang on", trad:"等一下兒", simp:"等一下儿", py:"děng yíxiàr"},
+      {en:"to wait", trad:"等", simp:"等", py:"děng"},
+      {en:"Got it.", trad:"知道了", simp:"知道了", py:"zhīdao le"},
+      {en:"to know; be aware of", trad:"知道", simp:"知道", py:"zhīdao"},
+      {en:"thanks", trad:"謝謝", simp:"谢谢", py:"xièxie"},
+      {en:"(suggestion/assumption particle)", trad:"吧", simp:"吧", py:"ba"},
+      {en:"correct; right", trad:"對", simp:"对", py:"duì"},
+      {en:"busy", trad:"忙", simp:"忙", py:"máng"},
+      {en:"to look at; to see; to watch", trad:"看", simp:"看", py:"kàn"},
+      {en:"television", trad:"電視", simp:"电视", py:"diànshì"},
+      {en:"to do", trad:"做", simp:"做", py:"zuò"},
+      {en:"to be online", trad:"上網", simp:"上网", py:"shàngwǎng"},
+      {en:"(on the phone) this is … speaking", trad:"我就是", simp:"我就是", py:"wǒ jiù shì"},
+      {en:"(polite M.W. for people)", trad:"位", simp:"位", py:"wèi"},
+      {en:"to leave a message (Mainland)", trad:"留言", simp:"留言", py:"liúyán"},
+      {en:"to leave a message (also TW)", trad:"留話", simp:"留话", py:"liúhuà"},
+      {en:"I’m sorry", trad:"對不起", simp:"对不起", py:"duìbuqǐ"},
+      {en:"when; at what time", trad:"什麼時候", simp:"什么时候", py:"shénme shíhòu"},
+      {en:"to return", trad:"回來", simp:"回来", py:"huílái"},
+      {en:"evening; night", trad:"晚上", simp:"晚上", py:"wǎnshang"},
+      {en:"to want; to desire", trad:"要", simp:"要", py:"yào"},
+      {en:"to; for; to give", trad:"給", simp:"给", py:"gěi"}
+    ],
+
+    // ---- 1-10 (Time & Schedules) ----
+    "Lesson 1-10": [
+      {en:"every day", trad:"每天", simp:"每天", py:"měitiān"},
+      {en:"every", trad:"每", simp:"每", py:"měi"},
+      {en:"day", trad:"天", simp:"天", py:"tiān"},
+      {en:"half", trad:"半", simp:"半", py:"bàn"},
+      {en:"to get up", trad:"起床", simp:"起床", py:"qǐchuáng"},
+      {en:"college; university", trad:"大學", simp:"大学", py:"dàxué"},
+      {en:"life", trad:"生活", simp:"生活", py:"shēnghuó"},
+      {en:"semester", trad:"學期", simp:"学期", py:"xuéqī"},
+      {en:"(M.W. for courses)", trad:"門", simp:"门", py:"mén"},
+      {en:"o’clock (on clock)", trad:"點", simp:"点", py:"diǎn"},
+      {en:"to sleep; go to bed", trad:"睡覺", simp:"睡觉", py:"shuìjiào"},
+      {en:"as early as; precisely", trad:"就", simp:"就", py:"jiù"},
+      {en:"not until; only then", trad:"才", simp:"才", py:"cái"},
+      {en:"a quarter (of an hour)", trad:"刻", simp:"刻", py:"kè"},
+      {en:"then; afterwards", trad:"然後", simp:"然后", py:"ránhòu"},
+      {en:"library", trad:"圖書館", simp:"图书馆", py:"túshūguǎn"},
+      {en:"afternoon", trad:"下午", simp:"下午", py:"xiàowǔ"},
+      {en:"to like", trad:"喜歡", simp:"喜欢", py:"xǐhuan"},
+      {en:"to play ball (sports)", trad:"打球", simp:"打球", py:"dǎqiú"},
+      {en:"ball", trad:"球", simp:"球", py:"qiú"},
+      {en:"to write", trad:"寫", simp:"写", py:"xiě"},
+      {en:"letter", trad:"信", simp:"信", py:"xìn"},
+      {en:"email", trad:"電子郵件", simp:"电子邮件", py:"diànzǐ yóujiàn"},
+      {en:"address", trad:"地址", simp:"地址", py:"dìzhǐ"},
+      {en:"year", trad:"年", simp:"年", py:"nián"},
+      {en:"month", trad:"月", simp:"月", py:"yuè"},
+      {en:"day (date/weekday word)", trad:"日", simp:"日", py:"rì"}
+    ],
+
+    // ---- 1-11 (Ordering Food) ----
+    "Lesson 1-11": [
+      {en:"red", trad:"紅", simp:"红", py:"hóng"},
+      {en:"tea", trad:"茶", simp:"茶", py:"chá"},
+      {en:"or (A or B?)", trad:"還是", simp:"还是", py:"háishi"},
+      {en:"still; also", trad:"還", simp:"还", py:"hái"},
+      {en:"green", trad:"綠", simp:"绿", py:"lǜ"},
+      {en:"waiter/waitress", trad:"服務員", simp:"服务员", py:"fúwùyuán"},
+      {en:"to sit", trad:"坐", simp:"坐", py:"zuò"},
+      {en:"Miss", trad:"小姐", simp:"小姐", py:"xiǎojiě"},
+      {en:"Mister", trad:"先生", simp:"先生", py:"xiānsheng"},
+      {en:"first", trad:"先", simp:"先", py:"xiān"},
+      {en:"to drink", trad:"喝", simp:"喝", py:"hē"},
+      {en:"cup (M.W.)", trad:"杯", simp:"杯", py:"bēi"},
+      {en:"ice; iced; with ice", trad:"冰", simp:"冰", py:"bīng"},
+      {en:"water", trad:"水", simp:"水", py:"shuǐ"},
+      {en:"bottle (M.W.)", trad:"瓶", simp:"瓶", py:"píng"},
+      {en:"cola", trad:"可樂", simp:"可乐", py:"kělè"},
+      {en:"noodle", trad:"麵", simp:"面", py:"miàn"},
+      {en:"dumpling (crescent-shaped)", trad:"餃子", simp:"饺子", py:"jiǎozi"},
+      {en:"plate (M.W.)", trad:"盤", simp:"盘", py:"pán"},
+      {en:"to stir-fry", trad:"炒", simp:"炒", py:"chǎo"},
+      {en:"ten", trad:"十", simp:"十", py:"shí"},
+      {en:"to order food", trad:"點菜", simp:"点菜", py:"diǎncài"},
+      {en:"bowl (M.W.)", trad:"碗", simp:"碗", py:"wǎn"},
+      {en:"soup", trad:"湯", simp:"汤", py:"tāng"},
+      {en:"pair (M.W.)", trad:"雙", simp:"双", py:"shuāng"},
+      {en:"chopstick", trad:"筷子", simp:"筷子", py:"kuàizi"}
+    ],
+
+    // ---- 1-18 (Core Vocab you approved) ----
+    "Lesson 1-18": [
+      {en:"train", trad:"火車", simp:"火车", py:"huǒchē"},
+      {en:"to travel", trad:"旅行", simp:"旅行", py:"lǚxíng"},
+      {en:"to be away from", trad:"離", simp:"离", py:"lí"},
+      {en:"school", trad:"學校", simp:"学校", py:"xuéxiào"},
+      {en:"far", trad:"遠", simp:"远", py:"yuǎn"},
+      {en:"only", trad:"只要", simp:"只要", py:"zhǐyào"},
+      {en:"minute", trad:"分鐘", simp:"分钟", py:"fēnzhōng"},
+      {en:"to ride", trad:"騎", simp:"骑", py:"qí"},
+      {en:"bicycle", trad:"自行車", simp:"自行车", py:"zìxíngchē"},
+      {en:"bus", trad:"公共汽車", simp:"公共汽车", py:"gōnggòngqìchē"},
+      {en:"to walk", trad:"走路", simp:"走路", py:"zǒulù"},
+      {en:"road", trad:"路", simp:"路", py:"lù"},
+      {en:"near", trad:"近", simp:"近", py:"jìn"},
+      {en:"the West", trad:"西部", simp:"西部", py:"xībù"},
+      {en:"first", trad:"先", simp:"先", py:"xiān"},
+      {en:"scenery", trad:"風景", simp:"风景", py:"fēngjǐng"},
+      {en:"ship", trad:"船", simp:"船", py:"chuán"},
+      {en:"the South", trad:"南部", simp:"南部", py:"nánbù"},
+      {en:"to hear of / it is said", trad:"聽說", simp:"听说", py:"tīngshuō"},
+      {en:"to hear", trad:"聽", simp:"听", py:"tīng"},
+      {en:"seaside", trad:"海邊", simp:"海边", py:"hǎibiān"},
+      {en:"scenery, view", trad:"景色", simp:"景色", py:"jǐngsè"},
+      {en:"altogether, in all", trad:"一共", simp:"一共", py:"yígòng"},
+      {en:"to rent; lease", trad:"租", simp:"租", py:"zū"}
+    ],
+    
+    // ---- 1-19 (Health & Medicine) ----
+    "Lesson 1-19": [
+      { en:"cold, flu",                  trad:"感冒", simp:"感冒", py:"gǎnmào" },
+      { en:"hungry; to starve",          trad:"餓",   simp:"饿",   py:"è" },
+      { en:"what happened; what's wrong",trad:"怎麼了", simp:"怎么了", py:"zěnmele" },
+      { en:"to be like; to seem",        trad:"好像", simp:"好像", py:"hǎoxiàng" },
+      { en:"comfortable; well",          trad:"舒服", simp:"舒服", py:"shūfu" },
+      { en:"to have a headache",         trad:"頭疼", simp:"头疼", py:"tóuténg" },   // first word only
+      { en:"to have a fever",            trad:"發燒", simp:"发烧", py:"fāshāo" },
+      { en:"to cough",                   trad:"咳嗽", simp:"咳嗽", py:"késou" },
+      { en:"to fall ill; get sick",      trad:"生病", simp:"生病", py:"shēngbìng" }, // first word only
+      { en:"exam",                       trad:"考試", simp:"考试", py:"kǎoshì" },
+      { en:"to review",                  trad:"復習", simp:"复习", py:"fùxí" },
+      { en:"therefore; consequently",    trad:"所以", simp:"所以", py:"suǒyǐ" },
+      { en:"doctor",                     trad:"醫生", simp:"医生", py:"yīshēng" },
+      { en:"to take medicine",           trad:"吃藥", simp:"吃药", py:"chīyào" },    // first word only
+      { en:"(particle de)",              trad:"地",   simp:"地",   py:"de" },
+      { en:"to rest",                    trad:"休息", simp:"休息", py:"xiūxi" },
+      { en:"to prepare",                 trad:"準備", simp:"准备", py:"zhǔnbèi" },
+      { en:"notes",                      trad:"筆記", simp:"笔记", py:"bǐjì" },
+      { en:"to thank; to be grateful",   trad:"感謝", simp:"感谢", py:"gǎnxiè" },
+      { en:"to deliver; escort; see off",trad:"送",   simp:"送",   py:"sòng" }
+    ]
+  }
+};
+
+console.log('VOCAB lessons loaded:', Object.keys(window.VOCAB.lessons).slice(-5));
